63a27294ea27f3cce6cfcadc78e84bce91131b99
Mariana Gutierrez Almaguer <marianagutierreza19@gmail.com>
2026-04-20 11:50:25 -0600

    Agrego añadir contactos

--- src/hackathon/agenda/service/Agenda.java
+++ src/hackathon/agenda/service/Agenda.java
@@ -1,4 +1,32 @@
 package hackathon.agenda.service;
 
+import java.util.HashSet;
+
 public class Agenda {
+    private Set<Contacto> contactos;
+    private int capacidad;
+
+    public Agenda() {
+        this.contactos = 10;
+        this.capacidad = new HashSet<>();
+    }
+
+    public Agenda(int capacidad) {
+        this.capacidad = capacidad;
+        this.capacidad = new HashSet<>();
+    }
+
+    // añadir contactos
+    public void anadirContacto(Contacto c) {
+        if (agendaLlena()) {
+            System.out.println("La agenda está llena");
+        } else if (existeContacto(c)) {
+            System.out.println("El nombre del contacto ya existe intenta colocando otro nombre para ese número");
+        } else {
+            contactos.add(c);
+            System.out.println("El contacto se añadido correctamente");
+        }
+    }
+
+
 }
